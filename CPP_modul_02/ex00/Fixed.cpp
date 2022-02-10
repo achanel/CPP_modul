73307a0dcad4b8f917308c78f77d056025e1de5d
achanel <achanel@student.21-school.ru>
2022-02-10 09:13:11 +0300

    fin02_00

---
+++ CPP_modul_02/ex00/Fixed.cpp
@@ -0,0 +1,35 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPoint(0)
+{
+    std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &fixed) : fixedPoint(fixed.fixedPoint)
+{
+    std::cout << "Copy constructor called\n";
+}
+
+Fixed &Fixed::operator=(const Fixed& fixed)
+{
+    std::cout << "Copy assignment operator called\n";
+    fixedPoint = fixed.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits() const
+{
+    std::cout << "getRawBits member function called\n";
+    return fixedPoint;
+}
+
+void    Fixed::setRawBits(int m_fixedPoint)
+{
+    std::cout << "setRawBits member function called\n";
+    fixedPoint = m_fixedPoint;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called\n";   
+}
---
+++ CPP_modul_02/ex00/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+private:
+    int                 fixedPoint;
+    static const int    bit = 8;
+public:
+    Fixed();
+    Fixed(const Fixed &copy);
+    Fixed & operator=(const Fixed &fixed);
+    int getRawBits() const;
+    void    setRawBits(int m_fixedPoint);
+    ~Fixed();
+};
+
+
+#endif
