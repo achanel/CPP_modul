f03ec95054065aaace7b5efa266b8c9b3e05e980
achanel <achanel@student.21-school.ru>
2022-02-14 17:08:03 +0300

    fin02_2

--- CPP_modul_02/ex03/bsp.cpp
+++ CPP_modul_02/ex03/bsp.cpp
@@ -8,13 +8,12 @@ float   dotProduct(Point p1, Point p2, Point p3)
 bool    bsp( Point const a, Point const b, Point const c, Point const point)
 {
     float   d1, d2, d3;
-    bool    neg;
-    bool    pos;
 
     d1 = dotProduct(point, a, b);
     d2 = dotProduct(point, b, c);
     d3 = dotProduct(point, c, a);
-    neg = d1 < 0 || d2 < 0 || d3 < 0;
-    pos = d1 > 0 || d2 > 0 || d3 > 0;
-    return !(neg && pos);
+    if ((d1 <= 0 && d2 <= 0 && d3 <= 0) || (d1 > 0 && d2 > 0 && d3 > 0))
+        return (1);
+    else
+        return (0);
 }
