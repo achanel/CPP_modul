f9d4649722d7f4c6bc5bb803f3ddc25180cab989
achanel <achanel@student.21-school.ru>
2022-03-02 08:36:40 +0300

    fin00

--- CPP_modul_08/ex00/easyfind.hpp
+++ CPP_modul_08/ex00/easyfind.hpp
@@ -1,4 +1,20 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+typename T::iterator    easyfind(T &container, int n)
+{
+    vector<int>::iterator   iter;
+    iter = find(container.begin(), container.end(), n);
+    if (iter != container.end())
+        return iter;
+    throw exception();
+}
+
 #endif
--- CPP_modul_08/ex00/main.cpp
+++ CPP_modul_08/ex00/main.cpp
@@ -1,6 +1,32 @@
 #include "easyfind.hpp"
 
+# define COLOR "\033[0m"
+# define YELLOW "\033[33m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define BLUE "\033[34m"
+
 int main(void)
 {
-    
+    ::vector<int> vector;
+    ::vector<int>::iterator vIter;
+    int n = -1;
+    cout << "n: " << n << endl;
+    for (int i = -20; i < 20; ++i)
+        vector.push_back(i);
+    for (vIter = vector.begin(); vIter != vector.end(); ++vIter)
+        cout << YELLOW << *vIter << COLOR << ' ';
+    cout << endl;
+    try
+    {
+        {
+            vIter = easyfind(vector, n);
+            cout << GREEN << "I found: " << n << COLOR << endl;
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << RED << "Not found" << COLOR << '\n';
+    }
+    return 0;
 }
